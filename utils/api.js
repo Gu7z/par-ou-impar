f0ce2b223cb5d20c1c6500330efd9d417477f021
Gu7z <gustavoferri13@gmail.com>
2021-03-14 13:56:35 -0400

    change env name

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiCaller = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_URL}/api`,
+  baseURL: `${process.env.URL}/api`,
 });
 
 export default apiCaller;
